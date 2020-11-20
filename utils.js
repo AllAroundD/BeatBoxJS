56b7c3076cb27ecd3d11abdc6dea41acea608d84
AllAroundD <AllAroundDWeb@gmail.com>
2020-11-20 13:48:48 -0500

    added colours and transitions

--- utils.js
+++ utils.js
@@ -4,7 +4,6 @@
 class Beat {
     constructor(audioSrc) {
         this.audio = new Audio(audioSrc)
-        console.log(this.audio)
     }
     play = () => {
         this.audio.currentTime = 0;
@@ -13,30 +12,46 @@ class Beat {
 
 }
 
-
-
 /**
  * Button class that keeps track of the button color based on a press
  */
 class Button {
     constructor(color, keyCode) {
+        this.color = color
+        this.keyCode = keyCode
+        this.element = document.getElementById(keyCode)
+        // console.log('element: ', this.element)
+        this.setButtonColorInHTML()
+        this.setATransitionEndListener()
     }
 
-    /**
+    setATransitionEndListener = () => {
+        this.element.addEventListener('transitionend', () => {
+            // console.log('transition ended')
+            this.deselect()
+        })
+    }
+
+    /**a
      * Set the button color based on color specified
      */
     setButtonColorInHTML = () => {
+        this.element.style.borderColor = this.color
     }
 
     /**
      * Select function to set the background color and boxShadow
      */
     select = () => {
+        this.element.style.backgroundColor = this.color
+        this.element.style.boxShadow = `0px 0px 17px 0px ${this.color}`
     }
 
     /**
      * Deselect function to reset background color and boxShadow
      */
     deselect = () => {
+        this.element.style.backgroundColor = "transparent"
+        this.element.style.boxShadow = "none"
     }
 }
